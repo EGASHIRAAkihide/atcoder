4148edc8895601d3a7e9da7f6a079696089a65ee
akihide.egashira <42921740+EGASHIRAAkihide@users.noreply.github.com>
2023-01-02 00:05:49 -0500

    feat: c部門の実装終了

--- abc278/c/c.cpp
+++ abc278/c/c.cpp
@@ -1,6 +1,4 @@
 #include <bits/stdc++.h>
-#include <stdio.h>
-#include <cmath>
 using namespace std;
 
 int main(){
@@ -16,7 +14,35 @@ int main(){
     Q >= 2 * pow(10, 5.0)
   );
 
-  cout << N << Q;
+  set<pair<int, int> > user;
+  user.insert({3, 2});
+
+  for (int i = 0; i < Q; i++)
+  {
+    int T, A, B;
+    cin >> T >> A >> B;
+
+    switch (T)
+    {
+      case 1:
+        user.insert({ A, B });
+        break;
+      case 2:
+        user.erase({ A, B });
+        break;
+      case 3:
+        if (user.count({ A, B }) && user.count({ B, A }))
+        {
+          cout << "Yes" << endl;
+        } else
+        {
+          cout << "No" << endl;
+        }
+        break;
+      default:
+        break;
+    }
+  };
 
   return 0;
 }
