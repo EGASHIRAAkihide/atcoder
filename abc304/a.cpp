d3aeef3c7fc88962eac6f89109139d5ce7af4aba
AKIHIDEEgashira <egashira.akihide@gmail.com>
2023-09-10 09:04:09 -0400

    feature: add new sections

---
+++ abc304/a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void){
+  int N;
+  string[] S;
+  int[] A;
+
+  do
+  {
+    cin >> N;
+  }
+  while (N < 1 || 100 < N);
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      
+    }
+  }
+
+  cout << N;
+}
